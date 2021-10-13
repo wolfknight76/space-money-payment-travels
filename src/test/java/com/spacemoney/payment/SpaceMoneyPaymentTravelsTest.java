9622fc831f32537e46617d1f8e94e86b31f9f553
wolfknight76 <wolfknight76>
2021-10-13 22:04:26 +0000

    Initial commit

---
+++ src/test/java/com/spacemoney/payment/SpaceMoneyPaymentTravelsTest.java
@@ -0,0 +1,21 @@
+package com.spacemoney.payment;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class SpaceMoneyPaymentTravelsTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
